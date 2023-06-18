19fbc10bf65434a2f556cc5d3ab559c3d388451a
lffelmann <67123057+lffelmann@users.noreply.github.com>
2023-06-18 22:36:32 +0200

    Deleted 2022 Nachtragstest because its the same as 2023 Mi

--- 2022/Nachtragstest/original/StarIterable.java
+++
@@ -1,8 +0,0 @@
-// An object with a method that returns a 'StarIterator'.
-// Please, do not change this interface definition!
-//
-public interface StarIterable extends Iterable<Star> {
-
-    // Returns an iterator over all 'Star' objects of the mobile.
-    StarIterator iterator();
-}
--- 2022/Nachtragstest/original/StickBreaksException.java
+++
@@ -1,7 +0,0 @@
-// An Exception thrown by 'Stick'.
-//
-public class StickBreaksException extends Exception {
-
-    //TODO: define missing parts of this class.
-
-}
